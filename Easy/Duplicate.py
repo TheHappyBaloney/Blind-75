27f84613ae4f385ea86f299b8d19518ed7b3ef49
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-04-10 21:58:28 +0530

    Create Duplicate.py

---
+++ Easy/Duplicate.py
@@ -0,0 +1,9 @@
+# 217. Contains Duplicate
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[ i + 1 ]:
+                return True
+        return False
