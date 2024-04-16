eaafeaa2f9df31c45e8c1f817955893b92f39748
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-04-16 06:57:30 +0000

    Chore: Catalan Numbers

---
+++ Uni-DSA/16-April-2024/Catalan.py
@@ -0,0 +1,54 @@
+'''
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert_recursive(node.right, value)
+
+# Usage example
+tree = BinarySearchTree()
+tree.insert(5)
+tree.insert(3)
+tree.insert(7)
+tree.insert(2)
+tree.insert(4)
+tree.insert(6)
+tree.insert(8)
+'''
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def count(n):
+    if n == 0:
+        return 1
+    else:
+        cat = factorial(2*n) / (factorial(n+1) * factorial(n))
+        return cat
+
+n = 6
+print(count(n))
