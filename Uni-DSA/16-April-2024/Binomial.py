8c3cbaa82149724aa42af222375c1e7158b0ae21
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-04-17 10:18:50 +0000

    Chore: Binomial Coefficient

--- Uni-DSA/16-April-2024/Binomial.py
+++ Uni-DSA/16-April-2024/Binomial.py
@@ -1,1 +1,23 @@
 # binomial coefficient of a number
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def binomial(n, k):
+    if k == 0:
+        return 1
+
+    elif k == 1:
+        return k
+
+    else:
+        binom = factorial(n) / (factorial(k) * factorial(n - k))
+        return binom
+
+
+# Usage of the function
+
+print(binomial(5, 2)) # 10
